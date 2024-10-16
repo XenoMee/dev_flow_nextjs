6b6ea6839b24c5f1fae027c9cd89786412860a01
Adrian Trandafir <adrian-stefan.trandafir@outlook.com>
2024-10-16 09:56:52 +0200

    add mobile navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Theme from './Theme';
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
@@ -37,7 +38,8 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        MobileNav
+
+        <MobileNav />
       </div>
     </nav>
   );
